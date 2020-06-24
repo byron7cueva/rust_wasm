74d894f557590afec67416646a06ddd843d8ee7f
byron7cueva <byron7cueva@gmail.com>
2020-06-24 15:41:05 -0500

    Bolerplait wasm

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
 const rust = import('./pkg/index');
+const canvas = document.getElementById('rustCanvas');
+const gl = canvas.getContext('webgl', { antialias: true });
 
-rust
+/* rust
   .then(m => m.say_hello_from_rust())
-  .catch(console.error);
+  .catch(console.error); */
+
+  rust
+  .then(m => {
+    if (!gl) {
+      alert('Fallo inicializar WebGL');
+      return;
+    }
+
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+    const FPS_THROTTLE = 1000.0 / 30.0; // milliseconds / frames
+    const dougsClient = new m.DougsClient();
+    const initialTime = Date.now();
+    var lastDrawTime = -1; // In milliseconds
+
+    function render () {
+      window.requestAnimationFrame(render);
+      const currTime = Date.now();
+
+      if (currTime >= lastDrawTime + FPS_THROTTLE) {
+        lastDrawTime = currTime;
+
+        // Para cuando el navegador se campie el tamaño
+        if (window.innerHeight != canvas.height || window.innerWidth != canvas.width) {
+          canvas.height = window.innerHeight;
+          canvas.width = window.innerWidth;
+          canvas.style.height = window.innerHeight;
+
+          canvas.width = window.innerWidth;
+          canvas.clientWidth = window.innerWidth;
+          canvas.style.width = window.innerWidth;
+
+          gl.viewport(0, 0, window.innerWidth, window.innerHeight);
+        }
+
+        let elapsedTime = currTime - initialTime;
+        // Rust Update call
+        dougsClient.update(elapsedTime, window.innerHeight, window.innerWidth);
+        // Rust Render call
+        dougsClient.render();
+      }
+    }
+
+    render();
+  })
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
+// Haciendo un wrap a la funcion console
 #[wasm_bindgen]
 extern "C" {
   #[wasm_bindgen(js_namespace = console)]
@@ -10,4 +11,28 @@ extern "C" {
 #[wasm_bindgen]
 pub fn say_hello_from_rust() {
   log("Hola from rust");
+}
+
+#[wasm_bindgen]
+pub struct DougsClient {
+
+}
+
+#[wasm_bindgen]
+impl DougsClient {
+
+  #[wasm_bindgen(constructor)]
+  pub fn new() -> Self {
+    log("New wasm hit");
+    Self {
+
+    }
+  }
+
+  pub fn update(&mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
+    Ok(())
+  }
+
+  pub fn render(&self) {
+  }
 }
